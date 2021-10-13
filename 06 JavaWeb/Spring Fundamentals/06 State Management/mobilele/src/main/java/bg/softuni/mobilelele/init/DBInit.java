808bf659386dd7f48e39ec1af5da632e52fd1a9c
rancho83a <rancho83a@gmail.com>
2021-10-13 16:10:14 +0300

    offer entity

--- 06 JavaWeb/Spring Fundamentals/06 State Management/mobilele/src/main/java/bg/softuni/mobilelele/init/DBInit.java
+++ 06 JavaWeb/Spring Fundamentals/06 State Management/mobilele/src/main/java/bg/softuni/mobilelele/init/DBInit.java
@@ -1,15 +1,11 @@
 package bg.softuni.mobilelele.init;
 
-import bg.softuni.mobilelele.model.entity.BrandEntity;
-import bg.softuni.mobilelele.model.entity.enums.CategoryEnum;
-import bg.softuni.mobilelele.model.entity.ModelEntity;
 import bg.softuni.mobilelele.service.BrandService;
 import bg.softuni.mobilelele.service.ModelService;
+import bg.softuni.mobilelele.service.OfferService;
 import bg.softuni.mobilelele.service.UserService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
 
 
 @Component
@@ -18,18 +14,20 @@ public class DBInit implements CommandLineRunner {
     private final UserService userService;
     private final BrandService brandService;
     private final ModelService modelService;
+    private final OfferService offerService;
 
-    public DBInit(UserService userService, BrandService brandService, ModelService modelService) {
+    public DBInit(UserService userService, BrandService brandService, ModelService modelService, OfferService offerService) {
         this.userService = userService;
         this.brandService = brandService;
         this.modelService = modelService;
+        this.offerService = offerService;
     }
 
     @Override
     public void run(String... args) throws Exception {
-
         brandService.initializeBrands();
         modelService.initializeModels();
         userService.initializeUsersAndRoles();
+        offerService.initializeOffers();
     }
 }
