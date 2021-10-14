3965afbb71a69f767d678537794761e9c0b2001f
rancho83a <rancho83a@gmail.com>
2021-10-14 14:08:56 +0300

    explain of every layer

---
+++ 06 JavaWeb/Spring Fundamentals/07 Spring Essentials/mobilele/src/main/java/bg/softuni/mobilelele/model/service/package-info.java
@@ -0,0 +1,8 @@
+package bg.softuni.mobilelele.model.service;
+
+/**
+ * This package contains service models.
+ *
+ *There is a class we pass to our service layer.
+ * Usually we populate it from the data comes into the  binding models.
+ */
