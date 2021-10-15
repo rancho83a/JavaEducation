5ec96bb6cc939654dbb5d164094f2199b3ed3832
rancho83a <rancho83a@gmail.com>
2021-10-15 14:48:31 +0300

    some examples model, modelview

--- 06 JavaWeb/Spring Fundamentals/07 Spring Essentials/mobilele/src/main/java/bg/softuni/mobilelele/web/UserRegistrationController.java
+++ 06 JavaWeb/Spring Fundamentals/07 Spring Essentials/mobilele/src/main/java/bg/softuni/mobilelele/web/UserRegistrationController.java
@@ -5,8 +5,10 @@ import bg.softuni.mobilelele.model.service.UserRegistrationServiceModel;
 import bg.softuni.mobilelele.service.UserService;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class UserRegistrationController {
@@ -19,19 +21,32 @@ public class UserRegistrationController {
         this.modelMapper = modelMapper;
     }
 
-    @GetMapping("/users/register")
+        @GetMapping("/users/register")
     public String registerUser(){
-
         return "auth-register";
     }
 
+//    //Variant za ModelView
+//    @GetMapping("/users/register")
+//    public String registerUser(Model model) {
+//        model.addAttribute("name", "value");
+//        return "auth-register";
+//    }
+
+    //Variant za ModelView
+//    @GetMapping("/users/register")
+//    public ModelAndView registerUser(ModelAndView modelAndView){
+//        modelAndView.setViewName("auth-register");
+//        return modelAndView;
+//    }
+
     @PostMapping("/users/register")
-    public String register(UserRegistrationBindingModel userRegistrationBindingModel){
+    public String register(UserRegistrationBindingModel userRegistrationBindingModel) {
 
 
         UserRegistrationServiceModel userRegistrationServiceModel = modelMapper.map(userRegistrationBindingModel, UserRegistrationServiceModel.class);
 
-        userService.registerAndLoginUser( userRegistrationServiceModel);
+        userService.registerAndLoginUser(userRegistrationServiceModel);
 
         return "redirect:/";
     }
