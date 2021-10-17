d030fb4bd01cc5b87ea6cc5a197a0671c641b5d6
rancho83a <rancho83a@gmail.com>
2021-10-17 16:20:27 +0300

    initialize 2 offers

--- 06 JavaWeb/Spring Fundamentals/07 Spring Essentials/mobilele/src/main/java/bg/softuni/mobilelele/service/impl/OfferServiceImpl.java
+++ 06 JavaWeb/Spring Fundamentals/07 Spring Essentials/mobilele/src/main/java/bg/softuni/mobilelele/service/impl/OfferServiceImpl.java
@@ -1,25 +1,64 @@
 package bg.softuni.mobilelele.service.impl;
 
 import bg.softuni.mobilelele.model.entity.OfferEntity;
+import bg.softuni.mobilelele.model.entity.enums.EngineEnum;
+import bg.softuni.mobilelele.model.entity.enums.TransmissionEnum;
 import bg.softuni.mobilelele.model.view.OffersSummaryView;
+import bg.softuni.mobilelele.repository.ModelRepository;
 import bg.softuni.mobilelele.repository.OfferRepository;
+import bg.softuni.mobilelele.repository.UserRepository;
 import bg.softuni.mobilelele.service.OfferService;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
 public class OfferServiceImpl implements OfferService {
     private final OfferRepository offerRepository;
+    private final ModelRepository modelRepository;
+    private final UserRepository userRepository;
 
-    public OfferServiceImpl(OfferRepository offerRepository) {
+    public OfferServiceImpl(OfferRepository offerRepository, ModelRepository modelRepository, UserRepository userRepository) {
         this.offerRepository = offerRepository;
+        this.modelRepository = modelRepository;
+        this.userRepository = userRepository;
     }
 
     @Override
     public void initializeOffers() {
-        //TODO
+        if (offerRepository.count() == 0) {
+            OfferEntity offer1 = new OfferEntity();
+            offer1
+                    .setModel(modelRepository.findById(1L).orElse(null))
+                    .setEngine(EngineEnum.GASOLINE)
+                    .setTransmission(TransmissionEnum.MANUAL)
+                    .setMileage(22500)
+                    .setPrice(BigDecimal.valueOf(14300))
+                    .setYear(2019)
+                    .setDescription("Used, but well services and in good condition.")
+                    .setSeller(userRepository.findByUsername("pesho")
+                            .orElse(null)) // or currentUser.getUserName()
+                    .setImageUrl(
+                            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcXp1KBpDKgYs6VqndkBpX8twjPOZbHV86yg&usqp=CAU");
+
+            OfferEntity offer2 = new OfferEntity();
+            offer2
+                    .setModel(modelRepository.findById(2L).orElse(null))
+                    .setEngine(EngineEnum.DIESEL)
+                    .setTransmission(TransmissionEnum.AUTOMATIC)
+                    .setMileage(209000)
+                    .setPrice(BigDecimal.valueOf(5500))
+                    .setYear(2000)
+                    .setDescription("After full maintenance, insurance, new tires...")
+                    .setSeller(userRepository.findByUsername("admin")
+                            .orElse(null)) // or currentUser.getUserName()
+                    .setImageUrl(
+                            "https://www.picclickimg.com/d/l400/pict/283362908243_/FORD-ESCORT-MK5-16L-DOHC-16v-ZETEC.jpg");
+
+            offerRepository.saveAll(List.of(offer1, offer2));
+        }
     }
 
     @Override
@@ -27,7 +66,8 @@ public class OfferServiceImpl implements OfferService {
 
 
         return offerRepository.findAll().stream()
-                .map(this::map).collect(Collectors.toList());
+                .map(this::map)
+                .collect(Collectors.toList());
     }
 
     private OffersSummaryView map(OfferEntity offerEntity){
