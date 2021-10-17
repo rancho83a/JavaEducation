c144e7b765f67a430dfc89cb196414e444afb06b
rancho83a <rancho83a@gmail.com>
2021-10-17 17:52:17 +0300

    getmapping details

--- 06 JavaWeb/Spring Fundamentals/07 Spring Essentials/mobilele/src/main/java/bg/softuni/mobilelele/web/OffersController.java
+++ 06 JavaWeb/Spring Fundamentals/07 Spring Essentials/mobilele/src/main/java/bg/softuni/mobilelele/web/OffersController.java
@@ -5,19 +5,27 @@ import bg.softuni.mobilelele.service.OfferService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class OffersController {
-    private  final OfferService offerService;
+    private final OfferService offerService;
 
     public OffersController(OfferService offerService) {
         this.offerService = offerService;
     }
 
     @GetMapping("/offers/all")
-    public String allOffers(Model model){
+    public String allOffers(Model model) {
         model.addAttribute("offers",
                 offerService.getAllOffers());
         return "offers";
     }
+
+    @GetMapping("/offers/{id}/details")
+    public String showOffer(@PathVariable Long id) {
+        return "details";
+    }
+
 }
+
