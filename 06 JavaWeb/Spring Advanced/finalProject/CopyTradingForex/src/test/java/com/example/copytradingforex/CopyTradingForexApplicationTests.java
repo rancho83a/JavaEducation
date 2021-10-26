234707bcac2820bc65595ce053ebef026955c56b
rancho83a <rancho83a@gmail.com>
2021-10-26 14:23:14 +0300

    start final project

---
+++ 06 JavaWeb/Spring Advanced/finalProject/CopyTradingForex/src/test/java/com/example/copytradingforex/CopyTradingForexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.copytradingforex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CopyTradingForexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
