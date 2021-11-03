3b4c99ab924b7ef5f5c41587e7c2a8c35bd5c7fc
rancho83a <rancho83a@gmail.com>
2021-11-03 20:58:33 +0200

    Error on valid error

--- 06 JavaWeb/Spring Advanced/02 Spring Security/mobilele/src/main/java/bg/softuni/mobilelele/web/UserLoginController.java
+++ 06 JavaWeb/Spring Advanced/02 Spring Security/mobilele/src/main/java/bg/softuni/mobilelele/web/UserLoginController.java
@@ -1,13 +1,11 @@
 package bg.softuni.mobilelele.web;
 
-import bg.softuni.mobilelele.model.binding.UserLoginBindingModel;
-import bg.softuni.mobilelele.model.service.UserLoginServiceModel;
-import bg.softuni.mobilelele.service.UserService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class UserLoginController {
@@ -15,6 +13,19 @@ public class UserLoginController {
     @GetMapping("/users/login")
     public String login() {
         return "auth-login";
+    }
+
+    @PostMapping("/users/login-error")
+    public String failedLogin(
+            @ModelAttribute(UsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_USERNAME_KEY)
+            String userName,
+            RedirectAttributes redirectAttributes
+    ){
+        redirectAttributes.addFlashAttribute("bad_credentials", true);
+        redirectAttributes.addFlashAttribute("username", userName);
+
+
+        return "redirect:/users/login";
     }
 
 }
