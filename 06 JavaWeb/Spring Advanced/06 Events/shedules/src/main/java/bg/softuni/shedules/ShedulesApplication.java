d87f08191c9c6f31372d25242facc5cf675ec221
rancho83a <rancho83a@gmail.com>
2021-11-15 22:33:27 +0200

    shedules initial commit

---
+++ 06 JavaWeb/Spring Advanced/06 Events/shedules/src/main/java/bg/softuni/shedules/ShedulesApplication.java
@@ -0,0 +1,13 @@
+package bg.softuni.shedules;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShedulesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShedulesApplication.class, args);
+	}
+
+}
