d0328c4830909e49c75d7a6d0235d33ec056ebe2
rancho83a <rancho83a@gmail.com>
2021-11-18 01:32:54 +0200

    initial commit

---
+++ 06 JavaWeb/Spring Advanced/07 Aspect Oriented Programming/booksClients/src/main/java/com/example/booksClients/BooksClientsApplication.java
@@ -0,0 +1,13 @@
+package com.example.booksClients;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksClientsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksClientsApplication.class, args);
+	}
+
+}
---
+++ 06 JavaWeb/Spring Advanced/07 Aspect Oriented Programming/booksClients/src/test/java/com/example/booksClients/BooksClientsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.booksClients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksClientsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
